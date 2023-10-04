84dbb35e4e180a0f81fdb50040a045a7aa84a9ed
Ilkhan-Sabirov <ilhansabirov@yandex.ru>
2023-10-04 21:04:00 +0300

    Добавлен красный квадрат в приложени

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="red-square"></div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+  .red-square {
+    background-color: red;
+    width: 40px;
+    height: 40px;
+  }
+</style>
